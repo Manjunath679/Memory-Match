61fcf4a25b99a54bbfb0f2422360c39b684aa5ca
Manjunath679 <you@example.com>
2021-08-17 13:12:59 +0530

    second commit

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express=require('express')
 const app=express()
 const path=require('path')
 
+const port = process.env.PORT || 3000;
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use('/',express.static(path.join(__dirname,'public')))
 
 
-app.listen(7123,()=>{
-    console.log("server is connected to http://localhost:7123")
+app.listen(port,()=>{
+    console.log('listening to port ' + port);
 })
